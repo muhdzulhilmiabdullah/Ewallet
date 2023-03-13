4cab790a5ddc3d1ca9b55c268deebc6a1ce3705e
Muhd. Zulhilmi Abdullah <muhdzulhilmiabdullah@gmail.com>
2023-03-13 08:37:30 +0800

    fix welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,10 @@
                 <div class="panel-body">
                     Hello there, welcome to xE-wallet's.<br>
                 </div>
+                <div class="panel-body">
+                    <a  class="btn btn-primary btn-sm" href="{{ url('/login') }}">Login</a>
+                    <a  class="btn btn-default btn-sm" href="{{ url('/register') }}">Register</a>
+                </div>
             </div>
         </div>
     </div>
